1ed0e8b2f195133dae07bdf3298e1acb5913c1a7
Gal Cohen <galcohen@galaxy-note9.ads.autodesk.com>
2019-08-22 17:25:47 +0300

    add support for forked version

--- README.md
+++ README.md
@@ -24,6 +24,18 @@ $ curl http://localhost:5000/task?time=30&block=true
 The default Q is configured with 10 seconds  
 Please note that if you pass time>10 duplications will occur
 
+#### Dead letter queue
+As for now, the implementation of roribio16/alpine-sqs will not support queueing to a dead letter. This is currently 
+working out of the box with real SQS running 
+
+if you like to work with DQL, please see my fork for its repo, where I configure DLQ in elasticmq.conf file 
+
+https://github.com/galCohen88/alpine-sqs
+
+and build the image 
+$ docker-compose -f docker-compose.build up -d --build
+
+
 #### SQS control panel
 to see SQS queues using web browser go to 
 
--- svc/tasks/tasks.py
+++ svc/tasks/tasks.py
@@ -1,19 +1,14 @@
 import time
 from celery import Celery
 from kombu import Queue
-from kombu.utils.url import quote
 
 app = Celery()
 
 # app.conf.broker_url = 'sqs://localhost:9324/queue'
 # app.conf.result_backend = 'redis://localhost:6379/0'
-# app.conf.broker_url = 'sqs://asdf:asdf@alpine-sqs:9324/queue'
 
-aws_access_key = quote('')
-aws_secret_key = quote('')
 
-app.conf.broker_url = 'sqs://{aws_access_key}:{aws_secret_key}@sqs.us-east-1.amazonaws.com/160043208412/celery-test'.format(
-    aws_access_key=aws_access_key, aws_secret_key=aws_secret_key)
+app.conf.broker_url = 'sqs://asdf:asdf@alpine-sqs:9324/queue/default'
 app.conf.result_backend = 'redis://redis:6379/0'
 
 
